203ed051a987a5cb20052791fbd78eee9b077d65
Fernanda Marques <marxsnanda07@gmail.com>
2025-08-14 17:20:52 -0300

    Primeiro commit do projeto

---
+++ src/app/cadastro/cadastro.page.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.page.html',
+  styleUrls: ['./cadastro.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule]
+})
+export class CadastroPage {
+  constructor() {}
+}
